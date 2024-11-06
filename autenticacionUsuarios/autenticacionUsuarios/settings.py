d571b95e2f9139c9856fe204891ebfab9fae4eca
Melany <go404756@uaeh.edu.mx>
2024-11-05 21:52:19 -0600

    Agregue la imagen del avatar, el boton para grabar y reproducir audio en la carpeta dashboard

--- autenticacionUsuarios/autenticacionUsuarios/settings.py
+++ autenticacionUsuarios/autenticacionUsuarios/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,6 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+#este lo agregue yo
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'autenticacionUsuarios/static')]
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+# Añade también
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
